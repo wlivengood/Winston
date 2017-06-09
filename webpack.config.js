12a788719c5795244df2faec4127f44a390529a9
Will Livengood <wlivengo@gmail.com>
2017-06-09 15:41:31 -0400

    restructured code for github pages

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const webpack = require('webpack');
 module.exports = {
     entry: './src/main.js',
     output: {
-        path: __dirname + '/bin',
+        path: __dirname,
         filename: 'app.bundle.js'
     },
     module: {
